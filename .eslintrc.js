0f16167e643504afa3e2d9449b932d16b0430e11
vladhuk <vladhookovskiy@gmail.com>
2021-04-24 21:08:37 +0300

    - Init project; - Set up prettier and eslint; - Set up git hooks;

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended', // Disable base rules
+    'plugin:@typescript-eslint/recommended', // Enable ts rules
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': WARN,
+
+    'class-methods-use-this': OFF,
+
+    '@typescript-eslint/interface-name-prefix': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    '@typescript-eslint/explicit-module-boundary-types': OFF,
+
+    'import/prefer-default-export': OFF,
+  },
+};
