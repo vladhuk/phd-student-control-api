145525598c4d0f87b87eb233101acbf8871403ef
vladhuk <vladhookovskiy@gmail.com>
2021-04-25 23:21:34 +0300

    - Add env file; - Set up typeorm config; - Remove app service;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        database: configService.get('DB_NAME'),
+        password: configService.get('DB_PASSWORD'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
