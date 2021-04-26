7930a2d93e8ef35755073686ac2ef4c9b007772f
vladhuk <vladhookovskiy@gmail.com>
2021-04-26 22:32:29 +0300

    - Update user model;

--- src/users/entities/User.entity.ts
+++ src/users/entities/User.entity.ts
@@ -5,6 +5,12 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ name: 'last_name' })
+  firstName: string;
+
+  @Column({ name: 'first_name' })
+  lastName: string;
+
   @Column({ unique: true })
   email: string;
 
