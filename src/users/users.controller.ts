0114aa7e51008bfdfd66a8603a7423608e98ecdc
vladhuk <vladhookovskiy@gmail.com>
2021-05-01 23:29:10 +0300

    - Add swagger api tag;

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { User } from 'src/_common/decorators/user.decorator';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
+@ApiTags('Users API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
