9e8b8f0b501174df1b0a31259a93006151360196
vladhuk <vladhookovskiy@gmail.com>
2021-05-02 03:16:19 +0300

    - Change API endpoints naming for current user;

--- src/scientific-directors/scientific-directors.controller.ts
+++ src/scientific-directors/scientific-directors.controller.ts
@@ -4,7 +4,7 @@ import { ForRole } from 'src/_common/decorators/for-role.decorator';
 import { Role } from 'src/_common/enums/role';
 import { ScientificDirectorsService } from './scientific-directors.service';
 
-@Controller('scientific-directors')
+@Controller('scientific-directors/me')
 @ApiTags('Scientific directors API')
 @ForRole(Role.SCIENTIFIC_DIRECTOR)
 export class ScientificDirectorsController {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,12 +4,12 @@ import { User } from 'src/_common/decorators/user.decorator';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('users/me')
 @ApiTags('Users API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('profile')
+  @Get()
   getProfile(@User() user: UserDto): UserDto {
     return user;
   }
